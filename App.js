e87bdd7f46f6ae01d7602feb8640cc0676cf77c4
tw9802 <148851212+tw9802@users.noreply.github.com>
2023-12-13 01:17:06 +0900

    Update App.js

--- App.js
+++ App.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [selectedPostId, setSelectedPostId] = useState(null);
+
   return (
+    <div className='App' style={{
+      display: 'flex',
+      flexDirection: 'column',
+    }}>
+      {selectedPostId ? (
+        <Post id={selectedPostId} resetPostID={() => setSelectedPostId(null)} />
+      ) : (
+        <div onClick={() => setSelectedPostId(1)}>게시글 1</div>
+      )}
+    </div>
+  );
+}
+
+    
+
+    
     <div className="App">
 
       <BoardHead></BoardHead>   
-
       <BoardNav></BoardNav>
 
       <Boarddetail></Boarddetail>
@@ -16,9 +33,12 @@ function App() {
 
       <Comment></Comment>
 
+      
+
+      
+
     </div>
-  );
-}
+
 
 function BoardHead(){
   return(
@@ -76,57 +96,7 @@ function Comment(){
   );
 }
 
- //TODO: 리액트 라우터 사용하여 URL Parameter로 포스트 아이디 받아오기
-const [selectedPostId, setSelectedPostId] = useState(null);
-
-
-return selectedPostId ? (
-  <Post id={selectedPostId} resetPostID={() => setSelectedPostID(null)} />
-) : (
-  <div className="App" style={{
-    display: 'flex',
-     flexDirection: 'column',
-  }}>
-     <div onClick={() => setSelectedPostID(1)}>게시글 1</div>
-     <div onClick={() => setSelectedPostID(2)}>게시글 2</div>
-     <div onClick={() => setSelectedPostID(3)}>게시글 3</div>
-     <div onClick={() => setSelectedPostID(4)}>게시글 4</div>
-   </div>   
-  );
-
-  function Post({
-    id,
-    resetPostId
-  }) {
-    const [title, setTitle] = useState('');
-    const [content, setContent] = useState('');
-  
-    //TODO: 현재 json 파일에서 데이터를 받아오고 있으나 Database를 접근해서 데이터를 가져와야함
-    fetch(`./sample-data/posts/${id}.json`, {
-      headers : {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-       }
-    })
-      .then((res) => {
-        return res.json();
-      })
-      .then((json) => {
-        setTitle(json.title);
-        setContent(json.content);
-      });
-  
-    //TODO: 돌아가기 버튼이 resetPostId를 부르는게 아니고 routing을 사용해서 게시판 url로 돌아가야한다
-    return (
-      <div>
-        <h1>{title}</h1>
-        <p>{content}</p>
-        <div onClick={() => resetPostId()}>돌아가기</div>
-      </div>
-    )
-  }
-  
-
+ 
 
 
  export default App;
